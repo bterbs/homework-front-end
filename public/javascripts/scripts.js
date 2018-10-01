e30d43cac7d5a7ee77262a724cbcd05a94ff7351
B Terbush <brittanyterbush@gmail.com>
2018-09-30 20:13:05 -0700

    Add script for search functionality, add event listener to search button, style.

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,3 +1,16 @@
+// DOCUMENT LOAD //
+$(function(){
+  console.log('Document ready - scripts are firing!')
+  getGifs();
+
+  $('#button-search').on('click', () => {
+    console.log('clicked the search button!')
+    let formData = $('#search-input').val();
+    searchGifs(formData);
+  });
+})
+
+// FUNCTION TO RETRIEVE GIFS //
 function getGifs(){
   $.ajax({
     url: "http://api.giphy.com/v1/gifs/search?q=silicon+valley&api_key=M2YPfZZz3lcvmxGykVK0ezr9ovXmQXzp&limit=25",
@@ -12,7 +25,7 @@ function getGifs(){
             $(`
               <div class="desktop-third tablet-half">
                 <div class="list-item">
-                  <img src=${gifUrl} class="desktop-third mobile-full">
+                  <img src=${gifUrl} class="desktop-third tablet-half mobile-full">
                 </div>
               </div>`)
           );
@@ -24,15 +37,39 @@ function getGifs(){
   })
 }
 
-$(function(){
-  console.log('Document ready - scripts are firing!')
-  getGifs();
-})
-
 // When the user scrolls the page to the bottom, load new GIFs automatically. //
 $(window).scroll(function() {
-    if($(window).scrollTop() == $(document).height() - $(window).height()) {
+    if($(window).scrollTop() + $(window).height() >= $(document).height()) {
       console.log('Getting more gifs!');
       getGifs()
     }
 });
+
+// SEARCH FUNCTIONALITY //
+function searchGifs(formTerm) {
+  console.log('search term is', formTerm)
+  $.ajax({
+    url: `http://api.giphy.com/v1/gifs/search?q=${formTerm}&api_key=M2YPfZZz3lcvmxGykVK0ezr9ovXmQXzp&limit=25`,
+    dataType: 'json'
+  })
+  .done(function(results) {
+    const gifNodes = [];
+    results.data.forEach(gif => {
+      const gifUrl = gif.images.fixed_height.url
+      gifNodes.push(
+            $(`
+              <div class="desktop-third tablet-half">
+                <div class="list-item">
+                  <img src=${gifUrl} class="desktop-third tablet-half mobile-full">
+                </div>
+              </div>`)
+          );
+        });
+
+        $('.section-gif-display').empty();
+
+        gifNodes.forEach(node => {
+          $('.section-gif-display').append(node);
+        });
+  })
+}

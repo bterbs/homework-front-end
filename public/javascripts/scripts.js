dc9a0257ab755ae85b4dbe888e964b413a9adc26
B Terbush <brittanyterbush@gmail.com>
2018-10-01 23:55:07 -0700

    Add info section for each gif and event handlers to display gif info on click

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,31 +1,39 @@
 // DOCUMENT LOAD //
 $(function(){
   console.log('Document ready - scripts are firing!')
-  getGifs();
+  getGifs(0);
 
   $('#button-search').on('click', () => {
     console.log('clicked the search button!')
     let formData = $('#search-input').val();
     searchGifs(formData);
   });
+
 })
 
 // FUNCTION TO RETRIEVE GIFS //
-function getGifs(){
+function getGifs(offset){
   $.ajax({
-    url: "http://api.giphy.com/v1/gifs/trending?api_key=M2YPfZZz3lcvmxGykVK0ezr9ovXmQXzp",
+    url: `http://api.giphy.com/v1/gifs/trending?api_key=M2YPfZZz3lcvmxGykVK0ezr9ovXmQXzp&offset=${offset}`,
     dataType: 'json'
   })
   .done(function(results) {
     console.log('here comes the gifs: ', results.data)
     const gifNodes = [];
     results.data.forEach(gif => {
-      const gifUrl = gif.images.fixed_height.url
+      const gifUrl = gif.images.fixed_height.url;
+      const gifSource = gif.url;
+      const gifUser = gif.username;
+      const gifRating = gif.rating;
       gifNodes.push(
             $(`
               <div class="desktop-third tablet-half">
-                <div class="list-item">
-                  <img src=${gifUrl} class="desktop-third tablet-half mobile-full">
+                <div class="list-item" style="background: url(${gifUrl}); background-size: cover;" class="desktop-third tablet-half mobile-full">
+                  <div class="d-none gif-info">
+                    <p>Rating: ${gifRating} </p>
+                    <p>Link: ${gifSource} </p>
+                    <p>Uploaded By: ${gifUser} </p>
+                  </div>
                 </div>
               </div>`)
           );
@@ -33,19 +41,29 @@ function getGifs(){
 
         gifNodes.forEach(node => {
           $('.section-gif-display').append(node);
+          $(node).on('click', () => {
+            console.log('clicked a div!')
+            var $thisdNone = $( event.target ).find('.d-none');
+            var $thisGifInfo = $( event.target ).find('.gif-info')
+
+            if ( $($thisdNone).css('visibility') == 'hidden' ) {
+              $($thisdNone).css('visibility','visible');
+            } else {
+              $('.d-none').css('visibility','hidden');
+            }
+
+            if ( $($thisGifInfo).css('background') == 'rgb(0, 170, 231, .8)') {
+              $($thisGifInfo).css('background', 'rgb(0, 170, 231, 0)')
+            } else {
+              $($thisGifInfo).css('background', 'rgb(0, 170, 231, .8)')
+            }
+          })
         });
+
   })
 }
 
-// When the user scrolls the page to the bottom, load new GIFs automatically. //
-$(window).scroll(function() {
-    if($(window).scrollTop() + $(window).height() >= $(document).height()) {
-      console.log('Getting more gifs!');
-      getGifs()
-    }
-});
-
-// SEARCH FUNCTIONALITY //
+// FUNCTION TO EXECUTE SEARCH //
 function searchGifs(formTerm) {
   console.log('search term is', formTerm)
   $.ajax({
@@ -56,11 +74,18 @@ function searchGifs(formTerm) {
     const gifNodes = [];
     results.data.forEach(gif => {
       const gifUrl = gif.images.fixed_height.url
+      const gifSource = gif.url;
+      const gifUser = gif.username;
+      const gifRating = gif.rating;
       gifNodes.push(
             $(`
               <div class="desktop-third tablet-half">
-                <div class="list-item">
-                  <img src=${gifUrl} class="desktop-third tablet-half mobile-full">
+                <div class="list-item" style="background: url(${gifUrl}); background-size: cover;" class="desktop-third tablet-half mobile-full">
+                  <div class="d-none gif-info">
+                    <p>Rating: ${gifRating} </p>
+                    <p>Link: ${gifSource} </p>
+                    <p>Uploaded By: ${gifUser} </p>
+                  </div>
                 </div>
               </div>`)
           );
@@ -70,6 +95,31 @@ function searchGifs(formTerm) {
 
         gifNodes.forEach(node => {
           $('.section-gif-display').append(node);
+          $('.section-gif-display').append(node);
+          $(node).on('click', () => {
+            console.log('clicked a div!')
+            var $thisdNone = $( event.target ).find('.d-none');
+            var $thisGifInfo = $( event.target ).find('.gif-info')
+
+            if ( $($thisdNone).css('visibility') == 'hidden' ) {
+              $($thisdNone).css('visibility','visible');
+            } else {
+              $('.d-none').css('visibility','hidden');
+            }
+
+            if ( $($thisGifInfo).css('background') == 'rgb(114, 140, 140, .8)') {
+              $($thisGifInfo).css('background', 'rgb(114, 140, 140, 0)')
+            } else {
+              $($thisGifInfo).css('background', 'rgb(114, 140, 140, .8)')
+            }
+          })
         });
   })
 }
+
+// When the user scrolls the page to the bottom, load new GIFs automatically. //
+$(window).scroll(function() {
+    if($(window).scrollTop() + $(window).height() >= $(document).height()) {
+      getGifs(25)
+    }
+});
